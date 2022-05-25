e1d180219abe8d385db973df442840970e34fd44
Giles Knap <giles.knap@diamond.ac.uk>
2022-05-25 07:27:52 +0100

    switch CI to helm 3.8.2

--- ghcr-deploy.sh
+++ ghcr-deploy.sh
@@ -5,11 +5,14 @@
 # run from this directory
 # REQUIRED: env CR_PAT must contain a valid GITHUB_TOKEN with needed permissions
 #
-IOC_CHART=ghcr.io/epics-containers/helm-ioc-lib
-VERSION=$(awk '/appVersion/{print $NF}' Chart.yaml)
+REGISTRY=oci://ghcr.io/epics-containers
+VERSION=$(awk '/appVersion:/{print $NF}' Chart.yaml)
+NAME=$(awk '/name:/{print $NF}' Chart.yaml)
+
+this_dir=$(realpath $(dirname $0))
 
 echo
-echo "Helm repo is ${IOC_CHART}"
+echo "Helm repo is ${REGISTRY}/${NAME}"
 echo "WARNING: this is not recommended. Push a Tag and let CI do this instead"
 read -p "Deploy IOC Helm Chart Library version ${VERSION} (Y/N)?" -n 1 -r
 echo
@@ -18,10 +21,10 @@ if [[ ${REPLY} =~ ^[Yy]$ ]]
 then
     echo $CR_PAT | helm registry login -u USERNAME --password-stdin ghcr.io/epics-containers
 
-    helm chart save . ${IOC_CHART}:${VERSION}
-    helm chart save . ${IOC_CHART}:latest
-    helm chart push ${IOC_CHART}:${VERSION}
-    helm chart push ${IOC_CHART}:latest
+    helm dependency update
+    helm package ${this_dir}
+    package=${NAME}-${VERSION}.tgz
+    helm push "$package" $REGISTRY
 fi
 
 
